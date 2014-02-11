02527911334221d07c0b2bcaaf59786dfe0b7dab
Tim Alander <tim.alander@gmail.com>
2014-02-11 18:18:00 -0500

    Initial commit -- transitioning from course private repo

---
+++ map.js
@@ -0,0 +1,306 @@
+var data = [{"Line":"Red","SouthID":"RALEN","NorthID":"RALEN","PlatformName":"ALEWIFE NB","StationName":"ALEWIFE","PlatformOrder":17,"StartOfLine":"FALSE","EndOfLine":"TRUE","Branch":"Trunk","Direction":"NB","stop_id":"place-alfcl","stop_code":null,"stop_name":"Alewife Station","stop_desc":null,"stop_lat":42.395428,"stop_lon":-71.142483},
+	{"Line":"Red","SouthID":"RDAVS","NorthID":"RDAVN","PlatformName":"DAVIS SB","StationName":"DAVIS","PlatformOrder":1,"StartOfLine":"TRUE","EndOfLine":"FALSE","Branch":"Trunk","Direction":"SB","stop_id":"place-davis","stop_code":null,"stop_name":"Davis Station","stop_desc":null,"stop_lat":42.39674,"stop_lon":-71.121815},
+	{"Line":"Red","SouthID":"RPORS","NorthID":"RPORN","PlatformName":"PORTER SB","StationName":"PORTER","PlatformOrder":2,"StartOfLine":"FALSE","EndOfLine":"FALSE","Branch":"Trunk","Direction":"SB","stop_id":"place-portr","stop_code":null,"stop_name":"Porter Square Station","stop_desc":null,"stop_lat":42.3884,"stop_lon":-71.119149},
+	{"Line":"Red","SouthID":"RHARS","NorthID":"RHARN","PlatformName":"HARVARD SB","StationName":"HARVARD","PlatformOrder":3,"StartOfLine":"FALSE","EndOfLine":"FALSE","Branch":"Trunk","Direction":"SB","stop_id":"place-harsq","stop_code":null,"stop_name":"Harvard Square Station","stop_desc":null,"stop_lat":42.373362,"stop_lon":-71.118956},
+	{"Line":"Red","SouthID":"RCENS","NorthID":"RCENN","PlatformName":"CENTRAL SB","StationName":"CENTRAL","PlatformOrder":4,"StartOfLine":"FALSE","EndOfLine":"FALSE","Branch":"Trunk","Direction":"SB","stop_id":"place-cntsq","stop_code":null,"stop_name":"Central Square Station","stop_desc":null,"stop_lat":42.365486,"stop_lon":-71.103802},
+	{"Line":"Red","SouthID":"RKENS","NorthID":"RKENN","PlatformName":"KENDALL SB","StationName":"KENDALL","PlatformOrder":5,"StartOfLine":"FALSE","EndOfLine":"FALSE","Branch":"Trunk","Direction":"SB","stop_id":"place-knncl","stop_code":null,"stop_name":"Kendall/MIT Station","stop_desc":null,"stop_lat":42.36249079,"stop_lon":-71.08617653},
+	{"Line":"Red","SouthID":"RMGHS","NorthID":"RMGHN","PlatformName":"CHARLES MGH SB","StationName":"CHARLES MGH","PlatformOrder":6,"StartOfLine":"FALSE","EndOfLine":"FALSE","Branch":"Trunk","Direction":"SB","stop_id":"place-chmnl","stop_code":null,"stop_name":"Charles/MGH Station","stop_desc":null,"stop_lat":42.361166,"stop_lon":-71.070628},
+	{"Line":"Red","SouthID":"RPRKS","NorthID":"RPRKN","PlatformName":"PARK SB","StationName":"PARK","PlatformOrder":7,"StartOfLine":"FALSE","EndOfLine":"FALSE","Branch":"Trunk","Direction":"SB","stop_id":"place-pktrm","stop_code":null,"stop_name":"Park St. Station","stop_desc":null,"stop_lat":42.35639457,"stop_lon":-71.0624242},
+	{"Line":"Red","SouthID":"RDTCS","NorthID":"RDTCN","PlatformName":"DOWNTOWN CROSSING SB","StationName":"DOWNTOWN CROSSING","PlatformOrder":8,"StartOfLine":"FALSE","EndOfLine":"FALSE","Branch":"Trunk","Direction":"SB","stop_id":"place-dwnxg","stop_code":null,"stop_name":"Downtown Crossing Station","stop_desc":null,"stop_lat":42.355518,"stop_lon":-71.060225},
+	{"Line":"Red","SouthID":"RSOUS","NorthID":"RSOUN","PlatformName":"SOUTH STATION SB","StationName":"SOUTH STATION","PlatformOrder":9,"StartOfLine":"FALSE","EndOfLine":"FALSE","Branch":"Trunk","Direction":"SB","stop_id":"place-sstat","stop_code":null,"stop_name":"South Station","stop_desc":null,"stop_lat":42.352271,"stop_lon":-71.055242},
+	{"Line":"Red","SouthID":"RBROS","NorthID":"RBRON","PlatformName":"BROADWAY SB","StationName":"BROADWAY","PlatformOrder":10,"StartOfLine":"FALSE","EndOfLine":"FALSE","Branch":"Trunk","Direction":"SB","stop_id":"place-brdwy","stop_code":null,"stop_name":"Broadway Station","stop_desc":null,"stop_lat":42.342622,"stop_lon":-71.056967},
+	{"Line":"Red","SouthID":"RANDS","NorthID":"RANDN","PlatformName":"ANDREW SB","StationName":"ANDREW","PlatformOrder":11,"StartOfLine":"FALSE","EndOfLine":"FALSE","Branch":"Trunk","Direction":"SB","stop_id":"place-andrw","stop_code":null,"stop_name":"Andrew Station","stop_desc":null,"stop_lat":42.330154,"stop_lon":-71.057655},
+	{"Line":"Red","SouthID":"RJFKS","NorthID":"RJFKN","PlatformName":"JFK SB","StationName":"JFK","PlatformOrder":12,"StartOfLine":"FALSE","EndOfLine":"FALSE","Branch":"Trunk","Direction":"SB","stop_id":"place-jfkred","stop_code":null,"stop_name":"JFK/UMass Station","stop_desc":null,"stop_lat":42.320685,"stop_lon":-71.052391},
+	{"Line":"Red","SouthID":"RSAVS","NorthID":"RSAVN","PlatformName":"SAVIN HILL SB","StationName":"SAVIN HILL","PlatformOrder":13,"StartOfLine":"FALSE","EndOfLine":"FALSE","Branch":"Ashmont","Direction":"SB","stop_id":"place-shmnl","stop_code":null,"stop_name":"Savin Hill Station","stop_desc":null,"stop_lat":42.31129,"stop_lon":-71.053331},
+	{"Line":"Red","SouthID":"RFIES","NorthID":"RFIEN","PlatformName":"FIELDS CORNER SB","StationName":"FIELDS CORNER","PlatformOrder":14,"StartOfLine":"FALSE","EndOfLine":"FALSE","Branch":"Ashmont","Direction":"SB","stop_id":"place-fldcr","stop_code":null,"stop_name":"Fields Corner Station","stop_desc":null,"stop_lat":42.300093,"stop_lon":-71.061667},
+	{"Line":"Red","SouthID":"RSHAS","NorthID":"RSHAN","PlatformName":"SHAWMUT SB","StationName":"SHAWMUT","PlatformOrder":15,"StartOfLine":"FALSE","EndOfLine":"FALSE","Branch":"Ashmont","Direction":"SB","stop_id":"place-smmnl","stop_code":null,"stop_name":"Shawmut Station","stop_desc":null,"stop_lat":42.29312583,"stop_lon":-71.06573796},
+	{"Line":"Red","SouthID":"RASHS","NorthID":"RASHN","PlatformName":"ASHMONT SB","StationName":"ASHMONT","PlatformOrder":16,"StartOfLine":"FALSE","EndOfLine":"TRUE","Branch":"Ashmont","Direction":"SB","stop_id":"place-asmnl","stop_code":null,"stop_name":"Ashmont Station","stop_desc":null,"stop_lat":42.284652,"stop_lon":-71.064489},
+	{"Line":"Red","SouthID":"RNQUS","NorthID":"RNQUN","PlatformName":"NORTH QUINCY SB","StationName":"NORTH QUINCY","PlatformOrder":13,"StartOfLine":"FALSE","EndOfLine":"FALSE","Branch":"Braintree","Direction":"SB","stop_id":"place-nqncy","stop_code":null,"stop_name":"North Quincy Station","stop_desc":null,"stop_lat":42.275275,"stop_lon":-71.029583},
+	{"Line":"Red","SouthID":"RWOLS","NorthID":"RWOLN","PlatformName":"WOLLASTON SB","StationName":"WOLLASTON","PlatformOrder":14,"StartOfLine":"FALSE","EndOfLine":"FALSE","Branch":"Braintree","Direction":"SB","stop_id":"place-wlsta","stop_code":null,"stop_name":"Wollaston Station","stop_desc":null,"stop_lat":42.2665139,"stop_lon":-71.0203369},
+	{"Line":"Red","SouthID":"RQUCS","NorthID":"RQUCN","PlatformName":"QUINCY CENTER SB","StationName":"QUINCY CENTER","PlatformOrder":15,"StartOfLine":"FALSE","EndOfLine":"FALSE","Branch":"Braintree","Direction":"SB","stop_id":"place-qnctr","stop_code":null,"stop_name":"Quincy Center Station","stop_desc":null,"stop_lat":42.251809,"stop_lon":-71.005409},
+	{"Line":"Red","SouthID":"RQUAS","NorthID":"RQUAN","PlatformName":"QUINCY ADAMS SB","StationName":"QUINCY ADAMS","PlatformOrder":16,"StartOfLine":"FALSE","EndOfLine":"FALSE","Branch":"Braintree","Direction":"SB","stop_id":"place-qamnl","stop_code":null,"stop_name":"Quincy Adams Station","stop_desc":null,"stop_lat":42.233391,"stop_lon":-71.007153},
+	{"Line":"Red","SouthID":"RBRAS","NorthID":"RBRAN","PlatformName":"BRAINTREE SB","StationName":"BRAINTREE","PlatformOrder":17,"StartOfLine":"FALSE","EndOfLine":"TRUE","Branch":"Braintree","Direction":"SB","stop_id":"place-brntn","stop_code":null,"stop_name":"Braintree Station","stop_desc":null,"stop_lat":42.2078543,"stop_lon":-71.0011385}];
+
+var currentLat = 0;
+var currentLng = 0;
+
+function initialize() {
+	var mapOptions = {
+		center: new google.maps.LatLng(42.3583, -71.0603),
+		zoom: 13,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+		};
+
+	var map = new google.maps.Map(document.getElementById("map_canvas"), 
+		mapOptions);
+
+	var stationMarkers = [];
+
+	addStationMarkers(map, stationMarkers);
+	getLocation(map, stationMarkers);
+}
+
+function addStationMarkers(map, stationMarkers) {
+	
+	var stationIcon = new google.maps.MarkerImage("icon.png", null, null, null, new google.maps.Size(32,32));
+	var stationCoords = [];
+
+	for(var i=0; i<data.length; i++) {
+		var stopPosition = new google.maps.LatLng(data[i].stop_lat,data[i].stop_lon);
+
+		stationCoords[i] = stopPosition;
+
+		var content = '<div id="content">'+
+    		'<div id="siteNotice">'+
+    		'</div>'+
+    		'<h2 id="firstHeading" class="firstHeading">'+ data[i].stop_name +'</h2>'+
+    		'<div id="bodyContent">'+
+    		'</div>'+
+    		'</div>';
+
+		var marker = new google.maps.Marker({
+      		position: stopPosition,
+      		map: map,
+      		icon: stationIcon,
+      		title: data[i].stop_name,
+      		content: content
+      	});
+
+      	stationMarkers[i] = marker;
+
+		var info = new google.maps.InfoWindow();
+
+		google.maps.event.addListener(marker, 'click', function() {
+  			info.open(map, this);
+  			info.setContent(this.content);
+		});
+	}
+
+	var stationLines = new google.maps.Polyline({
+    	path: stationCoords,
+    	strokeColor: "#FF0000",
+    	strokeOpacity: 0.7,
+    	strokeWeight: 5
+  	});
+
+  	stationLines.setMap(map);
+
+  	xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function()
+    	{
+            if (xmlhttp.readyState==4 && xmlhttp.status==200)
+            {
+                showTrainTimes(JSON.parse(xmlhttp.responseText), map, stationMarkers, info);
+            }
+        }
+    xmlhttp.open("GET", "http://mbtamap-cedar.herokuapp.com/mapper/redline.json", true);
+    xmlhttp.send(null);
+}
+
+function showTrainTimes (times, map, stationMarkers, info) {
+	for(var i=0; i<stationMarkers.length; i++){
+		var content = '<h2 id="firstHeading" class="firstHeading">'+ data[i].stop_name +'</h2>';
+		for(var j=0; j<times.length; j++){
+			if((times[j]["PlatformKey"] == (data[i].NorthID || data[i].SouthID)) && times[j]["InformatinType"] != "Arrived"){
+				content += '<div id="trip">Trip: ' + times[j]["Trip"] + ' Time remaining: ' + times[j]["TimeRemaining"] + '</div></br>';
+			}
+		}
+
+		stationMarkers[i].content = content;
+	}
+
+
+
+}
+
+function getLocation(map, stationMarkers) {
+	if(navigator.geolocation) 
+	{
+		navigator.geolocation.getCurrentPosition(function(position)
+		{
+
+			currentLat = position.coords.latitude;
+			currentLng = position.coords.longitude;
+
+			me = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+			var meMarker = new google.maps.Marker({
+      			position: me,
+      			map: map,
+      			//icon: stationIcon,
+      			title: "Current location"
+      			});
+
+      		map.panTo(me);
+
+      		showClosestStation(map, me, stationMarkers, meMarker);
+		});
+	}
+	else{
+		alert("Your browser does not support geolocation");
+	}
+}
+
+function showClosestStation(map, me, stationMarkers, meMarker) 
+{
+	distance = 0;
+	stationIndex = 0;
+
+	for(var i = 0; i < data.length; i++)
+	{
+		var lat1 = me.lat();
+		var lng1 = me.lng();
+		var lat2 = data[i].stop_lat;
+		var lng2 = data[i].stop_lon;
+
+		var d = getDistance(lat1, lng1, lat2, lng2);
+        if(i == 0)
+        {
+        	distance = d;
+        }
+        else
+        {
+        	if(d < distance)
+        	{
+        		distance = d;
+        		stationIndex = i;
+        	}
+        }
+	}
+
+	var currentLocationContent = '<div id="content">'+
+    	'<div id="siteNotice">'+
+    	'</div>'+
+    	'<h3 id="firstHeading" class="firstHeading"> You are here.</h3>'+
+    	'<div id="bodyContent"> The nearest station to you is <strong>' + stationMarkers[stationIndex].title +
+    	'</strong> which is ' + Math.round((distance/1.609)*100)/100 + ' miles away.'
+    	'</div>'+
+    	'</div>';
+
+	var currentLocationInfo = new google.maps.InfoWindow();
+	currentLocationInfo.open(map, meMarker);
+	currentLocationInfo.setContent(currentLocationContent);
+
+	var distanceLineCoords = [me, stationMarkers[stationIndex].position]
+
+	var distanceLine = new google.maps.Polyline ({
+    	path: distanceLineCoords,
+    	strokeColor: "#000000",
+    	strokeOpacity: 0.7,
+    	strokeWeight: 5
+  	});
+
+  	distanceLine.setMap(map);
+  	getLocationCarmenWaldo(map);
+}
+
+function getLocationCarmenWaldo(map) {
+	xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function()
+    	{
+            if (xmlhttp.readyState==4 && xmlhttp.status==200)
+            {
+                showCarmenWaldo(JSON.parse(xmlhttp.responseText), map);
+            }
+        }
+        xmlhttp.open("GET", "http://messagehub.herokuapp.com/a3.json", true);
+        xmlhttp.send(null);
+}
+
+function showCarmenWaldo(locationData, map) {
+
+	if(locationData.length > 0)
+	{
+		var position = new google.maps.LatLng(locationData[0]["loc"]["latitude"], locationData[0]["loc"]["longitude"]);
+ 	
+ 		var marker = new google.maps.Marker({
+      		position: position,
+      		map: map,
+      		});
+
+ 		if(locationData[0]["name"] == "Waldo")
+ 		{
+ 			marker.setIcon("waldo.png");
+ 			marker.setTitle("Waldo");
+ 		}
+ 		else
+ 		{
+			marker.setIcon("carmen.png");
+ 			marker.setTitle("Carmen");
+ 		}
+
+ 		distance = getDistance(currentLat, currentLng, locationData[0]["loc"]["latitude"], locationData[0]["loc"]["longitude"]);
+
+ 		var infoContent = '<div id="content">'+
+    		'<div id="siteNotice">'+
+    		'</div>' +
+    		'<div id="bodyContent">'+ marker.title +' is ' +
+    		Math.round((distance/1.609)*100)/100 + ' miles away.'
+    		'</div>'+
+    		'</div>';
+
+		var locationInfo = new google.maps.InfoWindow();
+		locationInfo.open(map, marker);
+		locationInfo.setContent(infoContent);
+
+ 		if(locationData.length == 2)
+ 		{
+ 			var position2 = new google.maps.LatLng(locationData[1]["loc"]["latitude"], locationData[1]["loc"]["longitude"]);
+ 	
+ 			var marker2 = new google.maps.Marker({
+      			position: position2,
+      			map: map,
+      			});
+
+ 			if(locationData[1]["name"] == "Waldo")
+ 			{
+ 				marker2.setIcon("waldo.png");
+ 				marker2.setTitle("Waldo");
+ 			}
+ 			else
+ 			{
+				marker2.setIcon("carmen.png");
+ 				marker2.setTitle("Carmen");
+ 			}
+
+ 			distance = getDistance(currentLat, currentLng, locationData[1]["loc"]["latitude"], locationData[1]["loc"]["longitude"]);
+
+ 			var infoContent2 = '<div id="content">'+
+    			'<div id="siteNotice">'+
+    			'</div>' +
+    			'<div id="bodyContent">'+ marker2.title +' is ' +
+    			Math.round((distance/1.609)*100)/100 + ' miles away.'
+    			'</div>'+
+    			'</div>';
+
+			var locationInfo2 = new google.maps.InfoWindow();
+			locationInfo2.open(map, marker2);
+			locationInfo2.setContent(infoContent2);
+ 		}
+ 	}
+}
+
+function getDistance(lat1, lng1, lat2, lng2) {
+	Number.prototype.toRad = function() 
+	{
+		return this * Math.PI / 180;
+	}
+
+	var radius = 6371;
+	var x1 = lat2-lat1;
+	var dLat = x1.toRad();
+
+	var x2 = lng2-lng1;
+
+	var dLng = x2.toRad();
+
+	var a = Math.sin(dLat/2) * Math.sin(dLat/2) + 
+   	Math.cos(lat1.toRad()) * Math.cos(lat2.toRad()) * 
+   	Math.sin(dLng/2) * Math.sin(dLng/2);
+
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+
+    var d = radius * c;
+
+    return d;
+}
